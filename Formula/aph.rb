39d506489f0aa3fdd8cc3eae908ec2af68e6de62
Vincent Auguin <vauguin-ext@deezer.com>
2022-06-18 13:16:19 +0200

    chore(aph): update to version 1.3.0

--- Formula/aph.rb
+++ Formula/aph.rb
@@ -1,8 +1,8 @@
 class Aph < Formula
   desc "Android Project Help (aph) is a CLI to help developers to quickly create and update their Android projects"
   homepage "https://github.com/VincentAuguin/android-project-helper"
-  url "https://github.com/VincentAuguin/android-project-helper/releases/download/1.2.0/aph.tar.gz"
-  sha256 "a3f0389f843495874a27651cf5496e00dcefe78ed8d57e521391e8fc905f6851"
+  url "https://github.com/VincentAuguin/android-project-helper/releases/download/1.3.0/aph.tar.gz"
+  sha256 "24c05dd380182d9fd92bb3dd8cb0690ae1430163baaece6e8528e1aa597cdea0"
   license "Apache-2.0"
 
   depends_on "gradle"
